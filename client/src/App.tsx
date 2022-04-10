bc813a26077ee1fa51f85b8b03b65c1e2a28cf98
lsh9955 <cqe9933@gmail.com>
2022-04-10 18:17:36 +0900

    main화면 구현중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UploadPage } from "../src/components/UploadPage/UploadPage";
+import { LandingPage } from "./components/LandingPage/LandingPage";
 import "./App.css";
+import { MainPage } from "./components/MainPage/MainPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/write" element={<UploadPage />} />
       </Routes>
     </Router>
---
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import videoBg from "../../assets/bgVideo.mp4";
+import mainIcon from "../../assets/bagMainIcon.svg";
+
+import LuggageIcon from "@mui/icons-material/Luggage";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { useTheme } from "@mui/material/styles";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+
+export const LandingPage = () => {
+  const theme = useTheme();
+  return (
+    <>
+      <div style={{ position: "relative", top: "0", left: "0", backgroundColor: "rgba(0, 0, 0, 0.8)", width: "100%", height: "100vh", overflow: "hidden" }}>
+        <div style={{ height: "40vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}></div>
+        <div style={{ height: "20vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <img src={mainIcon} style={{ margin: "0 0 3vw 0" }} />
+          <h1 style={{ fontSize: "40px", color: "white", margin: "0 0 0 0", lineHeight: "130%" }}>당신의 여행을 담아드립니다</h1>
+        </div>
+        <div style={{ height: "20vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Link to="/main" style={{ textDecoration: "none" }}>
+            <Button
+              size="medium"
+              sx={{
+                fontSize: "1.3rem",
+                backgroundColor: "none",
+                color: "white",
+
+                "&:hover": {
+                  backgroundColor: "skyblue",
+                  cursor: "pointer",
+                },
+              }}
+            >
+              Find Out More <LuggageIcon sx={{ color: "white", fontSize: 40, paddingLeft: "0.2rem" }} />
+            </Button>
+          </Link>
+        </div>
+
+        <video src={videoBg} muted loop autoPlay style={{ position: "absolute", zIndex: "-1", top: "0", left: "0", minWidth: "100%", minHeight: "100vh", objectFit: "contain" }}></video>
+      </div>
+    </>
+  );
+};
