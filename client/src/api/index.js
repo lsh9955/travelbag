5d3e45368944eb625f338063c168af88954d6250
lsh9955 <cqe9933@gmail.com>
2022-04-24 20:39:02 +0900

    배포 경로변경

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: "http://localhost:5000" });
-const API = axios.create({ baseURL: "http://34.219.85.175:5000" });
+// const API = axios.create({ baseURL: "http://34.219.85.175:5000" });
+const API = axios.create({ baseURL: "https://www.travelbag.tk" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
